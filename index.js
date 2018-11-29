950b122f22d535427ade4df04cd2dd939724f050
Edward Whelan <vroxing@gmail.com>
2018-11-29 07:58:01 -0500

    add in pg-sessions and express-sessions for login sec

--- index.js
+++ index.js
@@ -8,15 +8,34 @@ const db = require('./models/db');
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
-// const session = require('express-session');
-// const pgSession = require('connect-pg-simple')(session);
+const session = require('express-session');
+const pgSession = require('connect-pg-simple')(session);
 
 //Using Dependencies 
+app.use(session({
+  store: new pgSession({
+    pgPromise: db
+  }),
+  secret: 'g8j3lsa0sk2bbfhv5486nfgvge-387356!', // remember to adjust before deploying
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 30 * 24 * 60 * 60 * 1000 //Adjusts max time of session to 30 days
+  }
+}));
+
 app.use(express.static('public')); //all static files will be served from the public server
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+  let isLoggedIn = req.session.user ? true : false;
+  console.log(isLoggedIn);
+  next();
+})
+
+// REQUIRE MODELS AND VIEWS FUNCTIONS
+const page = require('./views/page')
 
 //ROUTES
 
@@ -24,7 +43,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //ROOT
 app.get('/', (req, res) =>
   res.send(
-    'Helloworld'
+    page(`<h3>sup</h3>`)
   ));
 
 
--- models/Users.js
+++ models/Users.js
@@ -11,11 +11,28 @@ class User {
       this.phonenumber = phonenumber,
       this.passHash = passHash
   }
+
+  // CREATE
   static addUser(displayname, emailaddress, phonenumber, password) {
     const salt = bcrypt.genSaltSync(saltRounds);
     const hash = bcrypt.hashSync(password, salt);
+    //SQL QUERY
+    return db.one(`
+    insert into users
+    (displayname, emailaddress, phonenumber, password)
+      values
+      ($1, $2, $3, $4)
+      returning id
+      `, [displayname, emailaddress, phonenumber, hash])
   }
+
+  //RETREIVE 
+
+  // UPDATE
+
+  // DELETE
+
 
 }
 
-module.exports = User
+module.exports = User;
